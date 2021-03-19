b809db06a0b41f045e1ddbb9e85b2c9e6e752a9a
eraleska <garettool@mail.ru>
2021-03-19 14:09:59 +0300

    created array

---
+++ src/code/DynamicArray.java
@@ -0,0 +1,9 @@
+package code;
+
+import java.util.ArrayList;
+
+public class DynamicArray {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+    }
+}
