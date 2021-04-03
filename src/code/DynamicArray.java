d8a420e4f4c9430c01860838026cd186c257ab58
eraleska <80881658+eraleska@users.noreply.github.com>
2021-04-03 12:41:47 +0300

    Delete src/code directory

--- src/code/DynamicArray.java
+++
@@ -1,9 +0,0 @@
-package code;
-
-import java.util.ArrayList;
-
-public class DynamicArray {
-    public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-    }
-}
